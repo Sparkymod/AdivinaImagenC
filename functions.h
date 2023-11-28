c3f6f0403ad76a80a12dfb415ac5c35f453a8edd
Sparkmod <rafadagher@gmail.com>
2023-11-28 15:08:00 -0400

    separando responsabilidades

---
+++ functions.h
@@ -0,0 +1,8 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+int listFiles(char* filenames[], const char* directoryPath);
+char* getBaseName(char* filename);
+void toLowerCase(char* str);
+
+#endif 
--- program.c
+++ program.c
@@ -1,14 +1,12 @@
 #define _CRT_SECURE_NO_WARNINGS
+
+#include "functions.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h>
 #include <time.h>
 #include <locale.h>
 #include <string.h>
-
-int listFiles(char* filenames[], const char* directoryPath);
-char* getBaseName(char* filename);
-void toLowerCase(char* str);
 
 struct Apuntes
 {
@@ -115,71 +113,4 @@ int main(void)
 	}
 
 	return 0;
-}
-
-// Lista los archivos a guarda los nombres en un arreglo.
-int listFiles(char* filenames[], const char* directoryPath) {
-
-	// Puntero que contiene el directorio especificado.
-	DIR* d = opendir(directoryPath);
-	if (d == NULL) {
-		perror("opendir failed");
-		return -1;
-	}
-
-	// esto es una estructura que define los campos de un archivo. 
-	// Necesitamos el campo de nombre(d_name) y el tipo(d_type) que ya este lo contiene.
-	struct dirent* dir;
-	int count = 0;
-
-	// Mientras tenga archivo la carpeta entonces guardara el nombre del archivo en la lista.
-	// DT_REG es una constante en C que significa que es un archivo regular -> https://www.gnu.org/software/libc/manual/html_node/Directory-Entries.html3
-	// Asi identificamos que lo que leemos es almenos un archivo y no una carpeta.
-	// TODO: Por el momento no tiene manejo de excepciones, si ponemos diferente tipos de archivos que no sea TXT u otra carpeta.
-	while ((dir = readdir(d)) != NULL) {
-		if (dir->d_type == DT_REG) {
-			filenames[count] = _strdup(dir->d_name);
-			count++;
-		}
-	}
-
-	closedir(d);
-	return count;
-}
-
-// Esta funcion directamente obtienes el nombre del archivo
-char* getBaseName(char* filename)
-{
-	// Este extrae el nombre de la ruta, delimitado con el ultimo / que aparezca, es decir, seria ruta/de/archivo.txt
-	// seria archivo.txt
-	char* base = strrchr(filename, '/');
-	if (!base)
-	{
-		base = filename;
-	}
-	else
-	{
-		base++;
-	}
-
-	// En base al anterior, entonces sacamos lo que esta delante del primer punto(.) que encuentre.
-	// Esto limita a que el nombre del archivo sea directo sin characteres especiales. 
-	// TODO: Habria que manejar mejor esta parte para aceptar un nombre mas amplio.
-	char* ext = strrchr(base, '.');
-	if (ext)
-	{
-		*ext = '\0';
-	}
-
-	return base;
-}
-
-// Esta funcion hace la simulacion de un ToLower() de C#. 
-// Convierte todo a minuscula para que se trate la respuesta como Case Insensitive.
-void toLowerCase(char* str) {
-	if (str == NULL) return;
-
-	for (int i = 0; str[i]; i++) {
-		str[i] = tolower((unsigned char)str[i]);
-	}
 }
